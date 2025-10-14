01263d14e8b9b275fb1829adaf53d5635f1bd28d
boukadia <mustapha.boukadia97@gmail.com>
2025-10-14 11:09:09 +0100

    faire des modificationn dans models

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const Schema=mongoose.Schema
+
 const appointmentSchema=new mongoose.Schema({
     patientId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
     doctorId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
-    disponibilite_id:{type:mongoose.Schema.Types.ObjectId,ref:'Disponibilite',required:false},
+    creneau: { type: Schema.Types.ObjectId, ref: 'Creneau', required: true },
     date:{type:Date,required:false},
     typeConsultation:{type:String,enum:['online','offline'],required:true},
     status:{type:String,enum:['pending','confirmed','cancelled'],default:'pending'},
--- models/Creneau.js
+++ models/Creneau.js
@@ -1,8 +1,12 @@
 const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const Disponibilite = require('./Disponibilite')
 const creneauSchema=new mongoose.Schema({
     heure_debut:{String,rquired:true},
     heure_fin:{ type: String, required: true },
     medecin: { type: Schema.Types.ObjectId, ref: 'User' },
-    patient: { type: Schema.Types.ObjectId, ref: 'User' }
+    patient: { type: Schema.Types.ObjectId, ref: 'User' },
+    disponibilite:{type:Schema.Types.ObjectId,ref:'Disponibilite',required:true},
+    statut:{type:String,enum:['libre','reserve'],default:'libre'}
 })
 module.exports=mongoose.model('Creneau',creneauSchema)
